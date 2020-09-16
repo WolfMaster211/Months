090e1d301adc2e0541ec68df06cb0d7164f84645
Opilane TTHK <opilane@tthk.ee>
2020-09-16 14:08:27 +0300

    Update

---
+++ script.js
@@ -0,0 +1,5 @@
+let today = new Date()
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+let selectYear = document.getElementById("year");
+let selectMonth = document.getElementById("month")
